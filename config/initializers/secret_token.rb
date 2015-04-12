1058323123b9abb149748d866fae6725f9e695a6
lg18889 <lg1889@nyu.edu>
2015-04-12 13:16:07 -0400

    update

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Fuckprogrma::Application.config.secret_key_base = '5f65d688f2ae087a026efff40502079e6c0cdb4d6c11ebbf391743348794a009f4bb3ee962ce2d56449b4020723b6483165b3c08a8006b494363a9c2719e89dd'
